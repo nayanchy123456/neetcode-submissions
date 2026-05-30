6b9eb5aeda3aab81b888bb70070c5ea40772d047
Nayan Chaudhary <154778478+nayanchy123456@users.noreply.github.com>
2026-05-30 15:56:15 +0545

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+
+            int cand = 0;
+            int count = 0;
+
+            for(int i=0; i<nums.length; i++){
+            
+                    if(count == 0){
+                           cand = nums[i];
+                           count++;
+                    }
+
+                    if(nums[i] != cand){
+                        count --;
+                    }
+                    else{
+                        count ++;
+                    }
+
+            }
+              return cand;
+    }
+
+
+}
